45832afb44ddcf0af913c783e614ddcabb8c06d0
jianqiu <wangjq@cn.ibm.com>
2015-12-09 00:47:51 +0800

    Adjusted bats pipeline

--- ci/scripts/upload-bat-release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bosh -t $director_target -u $director_user -p $director_password upload release bat-release-artifact/*.tgz || echo "Task exited with error code $?; assuming because release already exists and continuing..."
